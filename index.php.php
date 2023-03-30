f0438c22ea6824920db1db82676ad03b46d080a5
fagner39 <rengaf_22@hotmail.com>
2023-03-29 23:32:13 -0300

    Olá mundo PHP

---
+++ index.php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<title>Titulo da Aba</title>
+</head>
+<body>
+    <style>
+         /* tag style é um dos lugares para codificar css */
+         h1{
+           color: antiquewhite;
+           background-color: black;
+           text-align: center;
+         }
+    </style>
+     <h1 onclick="alert('Olá mundo Js')">Minha Página</h1>
+     <script>
+        document.write("Olá mundo JS 2")
+     </script>
+</body>
+</html>
