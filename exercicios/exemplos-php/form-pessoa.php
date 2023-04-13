e23af32d2a9d3665ad8d6e2202bbbd61128b1c9e
fagner39 <rengaf_22@hotmail.com>
2023-04-12 23:34:47 -0300

    exemplos-php

--- exercicios/exemplos-php/form-pessoa.php
+++ exercicios/exemplos-php/form-pessoa.php
@@ -7,14 +7,17 @@
 <body>
 <?php
 include 'conectar.php';
-$id = $nome = $email = $cpf = "";
+include 'validar-cpf.php';
+include 'escolaridade.php';
+$msgCpf = $id = $nome = $email = $cpf = $sexo = "";
 if($_SERVER["REQUEST_METHOD"] == "GET"){
     if (array_key_exists('id',$_GET)){
-      $id = $_GET['id'];
-      $pessoa = buscar($id);
-      $nome = $pessoa['nome'];
-      $email = $pessoa['email'];
-      $cpf = $pessoa['cpf'];
+        $id = $_GET['id'];
+        $pessoa = buscar($id);
+        $nome = $pessoa['nome'];
+        $email = $pessoa['email'];
+        $cpf = $pessoa['cpf'];
+        $sexo = $pessoa['sexo'];
     }
     if (array_key_exists('apagar',$_GET)){
         $apagar = $_GET['apagar'];
@@ -22,65 +25,104 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
         echo $msg;
     }
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $msg = "";
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $sexo = $_POST['sexo'];
+    $id = $_POST['id'];
+
+    $cpf = str_replace(".","",$cpf);
+    $cpf = str_replace("-","",$cpf);
+
+    if(validarCpf($cpf)){
+        if($id == ''){
+            $msg = incluir($nome, $email, $cpf, $sexo);
+        } else {
+            $msg = alterar($id, $nome, $email, $cpf, $sexo);
+        }
+    }else{
+        $msgCpf = "CPF inválido!";
+    }
+    
+    echo $msg;
+}
+
 ?>
+
 <form action="form-pessoa.php" method="post">
-      <input type="hidden" name= "id" value="<?php echo $id; ?>">
+    <input type="hidden" name="id"  value="<?php echo $id; ?>">
     <h1>Formulário de Pessoa</h1>
     Nome: <br>
-    <input type="text" name="nome" value="<?php echo $nome; ?>"><br>
+    <input type="text" name="nome" value="<?php echo $nome; ?>" required><br>
     E-mail: <br>
-    <input type="text" name="email" value="<?php echo $email; ?>"><br>
-    Cpf: <br>
-    <input type="text" name="cpf" value="<?php echo $cpf; ?>"><br>
+    <input type="email" name="email" value="<?php echo $email; ?>" required><br>
+    CPF:<?php echo $msgCpf; ?> <br>
+    <input type="text" name="cpf" value="<?php echo $cpf; ?>" required><br>
+    <!DOCTYPE html>
+<html>
+<body>
+<br>
+<form action="/action_page.php">
+  <label for="class">escolaridade:</label>
+  <select id="class" name="class">
+    <option value="Fundamental - Incompleto">Fundamental - Incompleto</option>
+    <option value="Fundamental - Completo">Fundamental - Completo</option>
+    <option value="Médio - Incompleto">Médio - Incompleto</option>
+    <option value="Médio - Completo">Médio - Completo</option>
+    <option value="Superior - Incompleto">Superior - Incompleto</option>
+    <option value="Superior - completo">Superior - completo</option>
+  </select>
+</form><br>
+<label for="pwd">senha :</label>
+<input type="senha" id="pwd" name="pwd">
+<br><br>
+<label for="pwd">confirme:</label>
+<input type="confirmar" id="pwd" name="pwd">
+<br>
+    Sexo: <br>
+    <input type="radio" name="sexo" value="m" required <?php if($sexo == "m") echo "checked"; ?>>Masculino
+    <input type="radio" name="sexo" value="f" required <?php if($sexo == "f") echo "checked"; ?>>Feminino
+    <br>
     <br>
     <input type="submit" value="Gravar">
     <a href="form-pessoa.php">
-    <input type="button" value="novo">
+    <input type="button" value="Novo">
     </a>
-    
-    
+</form>
 
-    
-</form> 
-<?php
- //onclick="window.location.replace('form-pessoa.php');">
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$cpf = $_POST['cpf'];
 
-$id = $_POST['id'];
-if($id == ''){
-    $msg = incluir($nome, $email, $cpf);
-}else{
-    $msg = alterar($id, $nome, $email, $cpf);
-}
 
-echo $msg; 
-}
-
-?>
 <br>
-<table border="l">
-   <tr>
+<table border="1">
+    <tr>
         <th>Id</th>
         <th>Nome</th>
         <th>Email</th>
-        <th>cpf</th>
-    </tr>   
+        <th>CPF</th>
+        <th>Sexo</th>
+    </tr><br>
     <?php
     $dados = listar();
     while ($linha = $dados->fetch_assoc()) {
-       echo "<tr>";
-       echo "<td>".$linha['id']."</td>";
-       echo "<td>".$linha['nome']."</td>";
-       echo "<td>".$linha['email']."</td>";
-       echo "<td>".$linha['cpf']."</td>";
-       echo "<td><a href='form-pessoa.php?id=".$linha['id']."'>Editar</a></td>";
-       echo "<td><a href='form-pessoa.php?apagar=".$linha['id']."'>Apagar</a></td>";
-       echo "</tr>";    
+        echo "<tr>";
+        echo "<td>".$linha['id']."</td>";
+        echo "<td>".$linha['nome']."</td>";
+        echo "<td>".$linha['email']."</td>";
+        echo "<td>".$linha['cpf']."</td>";
+        echo "<td>".$linha['sexo']."</td>";
+        echo "<td><a href='form-pessoa.php?id=".$linha['id']."'>Editar</a></td>";
+        echo "<td><a onclick='return apagar(".$linha['id'].");' href='form-pessoa.php?apagar=".$linha['id']."'>Apagar</a></td>";
+        echo "</tr>";
     }
     ?>
-</table>    
+    <script>
+        function apagar(id){
+            return confirm("Deseja Apagar o registro ID("+id+")?");
+        }
+    </script>
+</table>
 </body>
-</html>            
+</html>
